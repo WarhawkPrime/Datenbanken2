c4500811ef820b4dfff96657abe49dce5c04df46
strehamm <24031996Rh!>
2020-05-15 10:26:07 +0200

    Corrected Implementation

--- src/main/java/de/hda/fbi/db2/stud/impl/Lab01Imp.java
+++ src/main/java/de/hda/fbi/db2/stud/impl/Lab01Imp.java
@@ -1,38 +1,39 @@
 package de.hda.fbi.db2.stud.impl;
 
 import de.hda.fbi.db2.api.Lab01Data;
-
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.List;
 
 public class Lab01Imp extends Lab01Data {
-    @Override
-    public List<?> getQuestions() {
-        return null;
+  @Override
+  public List<?> getQuestions() {
+    return null;
+  }
+
+  @Override
+  public List<?> getCategories() {
+    return null;
+  }
+
+  @Override
+  public void loadCsvFile(List<String[]> additionalCsvLines) {
+    String row;
+    System.out.println(System.getProperty("user.dir"));
+    try {
+      BufferedReader csvReader = new BufferedReader(new FileReader(
+              System.getProperty("user.dir") + "/src/main/resources/Wissenstest_sample200.csv"));
+
+      while ((row = csvReader.readLine()) != null) {
+        String[] data = row.split(";");
+        // do something with the data
+      }
+
+      csvReader.close();
+    } catch (IOException e) {
+      e.printStackTrace();
     }
 
-    @Override
-    public List<?> getCategories() {
-        return null;
-    }
-
-    @Override
-    public void loadCsvFile(List<String[]> additionalCsvLines) {
-        String row;
-
-        try {
-            BufferedReader csvReader = new BufferedReader(new FileReader(/src/main/));
-            while ((row = csvReader.readLine()) != null) {
-                String[] data = row.split(",");
-                // do something with the data
-            }
-
-            csvReader.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-    }
+  }
 }
