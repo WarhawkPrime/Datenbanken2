2489a8260b0c11ef40a8f80d176110fe5d6d1868
strehamm <24031996Rh!>
2020-05-18 14:57:24 +0200

    fixed Checkstyle in Category

--- src/main/java/de/hda/fbi/db2/stud/entity/Category.java
+++ src/main/java/de/hda/fbi/db2/stud/entity/Category.java
@@ -1,17 +1,22 @@
 package de.hda.fbi.db2.stud.entity;
 
-import java.util.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Category {
-  private
-  String name;
-  ArrayList<Question> questions;
+  private String name;
+  private ArrayList<Question> questions;
 
   public Category(String name) {
     questions = new ArrayList<Question>();
     this.name = name;
   }
 
+  /**
+   * Adds a question.
+   * @param question Question to be added
+   */
   public void add_question(Question question) {
     try {
       questions.add(question);
@@ -20,14 +25,27 @@ public class Category {
     }
   }
 
+  /**
+   * Get question at given index.
+   * @param index Position of the question to be returned
+   * @return Question at given index
+   */
   public Question get_question(int index) {
     return questions.get(index);
   }
 
   //========== getter and setter ==========
-  public String get_name() {return this.name;}
-  public List<Question> get_questions() {return this.questions;}
-  public void set_name(String name) {this.name = name;}
+  public String get_name() {
+    return this.name;
+  }
+
+  public List<Question> get_questions() {
+    return this.questions;
+  }
+
+  public void set_name(String name) {
+    this.name = name;
+  }
 
 
 
